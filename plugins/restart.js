e97126ba3b84812f76bda6e72be3b343e71ac609
DybyTech <dybytech00@gmail.com>
2025-05-12 06:14:49 -0400

    MEGALODON-MD

---
+++ plugins/restart.js
@@ -0,0 +1,24 @@
+const { cmd } = require("../command");  
+const { sleep } = require("../lib/functions");  
+
+cmd({  
+    pattern: "restart",  
+    desc: "Restart MEGALODON-MD",  
+    category: "owner",  
+    filename: __filename  
+},  
+async (conn, mek, m, { reply, isCreator }) => {  
+    try {  
+        if (!isCreator) {  
+            return reply("Only the bot owner can use this command.");  
+        }  
+
+        const { exec } = require("child_process");  
+        reply("megalodon Restarting...");  
+        await sleep(1500);  
+        exec("pm2 restart all");  
+    } catch (e) {  
+        console.error(e);  
+        reply(`${e}`);  
+    }  
+});
