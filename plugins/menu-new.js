d43234a2527cbec711b586bbd7a952823dec4f07
DybyTech <dybytech00@gmail.com>
2025-05-12 06:56:09 -0400

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -7,7 +7,7 @@ cmd({
     pattern: "menu",
     desc: "Show interactive menu system",
     category: "menu",
-    react: "🧾",
+    react: "💫",
     filename: __filename
 }, async (conn, mek, m, { from, reply }) => {
     try {
