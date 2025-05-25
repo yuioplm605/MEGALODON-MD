978ee7f4d95eab29ad3ec3e1e1fee573fb8fd9e6
DybyTech <dybytech00@gmail.com>
2025-05-25 12:41:53 -0400

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363372853772240@newsletter',
+                        newsletterJid: '120363401051937059@newsletter',
                         newsletterName: "Privacy Settings",
                         serverMessageId: 143
                     }
