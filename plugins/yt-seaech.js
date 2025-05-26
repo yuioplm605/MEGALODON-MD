62d82f85b70ce42968ce56d614bc2878816fa6ef
DybyTech <dybytech00@gmail.com>
2025-05-26 03:50:58 -0400

    Update yt-seaech.js

--- plugins/yt-seaech.js
+++ plugins/yt-seaech.js
@@ -9,7 +9,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 cmd({
     pattern: "yts",
     alias: ["ytsearch"],
-    use: '.yts jawad',
+    use: '.yts DybyTech',
     react: "🔎",
     desc: "Search and get details from youtube.",
     category: "search",
