3321914e3de252936685a82a381b462503bf4ee9
DybyTech <dybytech00@gmail.com>
2025-05-27 01:16:21 -0400

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,5 +1,6 @@
 const { cmd } = require('../command');
 const os = require("os");
+const axios = require("axios");
 const { runtime } = require('../lib/functions');
 const config = require('../config');
 
@@ -26,6 +27,8 @@ async (conn, mek, m, { from, sender, reply }) => {
 > ╰────────────────────◉
 > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʏʙʏ ᴛᴇᴄʜ*`;
 
+        const thumbnailBuffer = (await axios.get("https://files.catbox.moe/rful77.jpg", { responseType: "arraybuffer" })).data;
+
         await conn.sendMessage(from, {
             image: { url: `https://files.catbox.moe/frns4k.jpg` },
             caption: status,
@@ -33,10 +36,15 @@ async (conn, mek, m, { from, sender, reply }) => {
                 mentionedJid: [m.sender],
                 forwardingScore: 1000,
                 isForwarded: true,
-                forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363401051937059@newsletter',
-                    newsletterName: '𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃',
-                    serverMessageId: 143
+                externalAdReply: {
+                    title: "MEGALODON-MD 🥀",
+                    body: "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʏʙʏ ᴛᴇᴄʜ*",
+                    mediaType: 1,
+                    renderLargerThumbnail: true,
+                    thumbnail: thumbnailBuffer,
+                    mediaUrl: "https://files.catbox.moe/frns4k.jpg",
+                    sourceUrl: "https://wa.me/message/yourself",
+                    showAdAttribution: true
                 }
             }
         }, { quoted: mek });
