a0b652a64990e0525a6926b4ea78919510f0f2e9
DybyTech <dybytech00@gmail.com>
2025-05-30 07:31:27 -0400

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -7,13 +7,13 @@ cmd({
   on: "body"
 },
 async (conn, mek, m, { from, body }) => {
-    const filePath = path.join(__dirname, '../assets/autosticker.json');
+    const filePath = path.join(__dirname, '../DybyTech/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             if (config.AUTO_STICKER === 'true') {
-                const stickerPath = path.join(__dirname, '../assets/autosticker', data[text]);
+                const stickerPath = path.join(__dirname, '../DybyTech/autosticker', data[text]);
 
                 if (fs.existsSync(stickerPath)) {
                     const stickerBuffer = fs.readFileSync(stickerPath);
