455a38c2f9900230457af0eb1ef4d5ee3ff44778
DybyTech <dybytech00@gmail.com>
2025-05-31 20:14:09 -0400

    Update alive2.js

--- plugins/alive2.js
+++ plugins/alive2.js
@@ -9,7 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.LIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
