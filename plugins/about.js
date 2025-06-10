d41ad205c6382154a3d0a8717a60b68bd6a2f638
DybyTech <dybytech00@gmail.com>
2025-06-10 02:52:11 -0400

    Create about.js

---
+++ plugins/about.js
@@ -0,0 +1,57 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    alias: "dev",
+    react: "👑",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `
+*╭┈───────────────•*
+*𝗁𝗂 𝖽𝖾𝖺𝗋 👋 ${pushname}*
+*╰┈───────────────•*
+*╭┈───────────────•*
+*│  ◦* *ᴄʀᴇᴀᴛᴇᴅ ʙʏ: ᴅʏʙʏ ᴛᴇᴄʜ*
+*│  ◦* *ʀᴇᴀʟ ɴᴀᴍᴇ➠ Dyby*
+*│  ◦* *ɴɪᴄᴋɴᴀᴍᴇ➠ DybyTech*
+*│  ◦* *ᴀɢᴇ➠ ❌*
+*│  ◦* *ᴄɪᴛʏ➠ Lɪʙʀᴇᴠɪʟʟᴇ*
+*│  ◦* *ᴀ ᴘᴀꜱꜱɪᴏɴᴀᴛᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ᴅᴇᴠ*
+*╰┈───────────────•*
+
+*[ • ᴍᴇɢᴀʟᴏᴅᴏɴ-ᴍᴅ - ᴘʀᴏᴊᴇᴄᴛ • ]*
+
+*╭┈───────────────•*
+*│  ◦* *▢➠DybyTech x Wasi_Tech*
+*│  ◦* *▢➠ᴏɴʟʏ 2 ᴅᴇᴠᴇʟᴏᴘᴇʀ*
+*╰┈───────────────•*
+
+*•────────────•⟢*
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅʏʙʏ ᴛᴇᴄʜ*
+*•────────────•⟢*
+`
+await conn.sendMessage(from, {
+    image: { url: 'https://files.catbox.moe/vmqovi.jpg' },
+    caption: about,
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363401051937059@newsletter', // ou ton JID actuel
+            newsletterName: '𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃',
+            serverMessageId: 143
+        }
+    }
+}, { quoted: mek })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
